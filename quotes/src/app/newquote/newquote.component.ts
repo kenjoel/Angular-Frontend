4477f4ec43491b58880fd8b1220e1e6b95542589
Ken-muigai <kenmuigai980@gmail.com>
2020-08-23 11:41:31 +0300

    succeeding commits

--- quotes/src/app/newquote/newquote.component.ts
+++ quotes/src/app/newquote/newquote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { quotes } from "../quotes"
 
 @Component({
   selector: 'app-newquote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newquote.component.css']
 })
 export class NewquoteComponent implements OnInit {
+  @Output() newQuote = new EventEmitter<quotes>();
+
+  quoteHolder = new quotes("","","");
+
+  renderContentToParent(){
+    this.newQuote.emit(this.quoteHolder)
+  }
+
+
 
   constructor() { }
 
