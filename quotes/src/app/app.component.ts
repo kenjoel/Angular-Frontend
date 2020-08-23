a8a15feb414eadec584659b2654ca8a13b975cf1
Ken-muigai <kenmuigai980@gmail.com>
2020-08-23 11:22:41 +0300

    succeeding commits

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { quotes } from './quotes'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
+  quotes: quotes[] = [
+    new quotes("for everything that happens, there's a reason","socratis", "kenjoel"),
+    new quotes("a russian wedding should be called a soviet union", "lex fridman", "Pres. Joel Muigai")
+  ]
+
+  showDetails(index){
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+  
+  addQuote(quote){
+    if(quote.quote && quote.author && quote.publisher){
+      this.quotes.push(quote)
+    }else{
+      quote.preventDefault();
+    }
+    
+  }
 }
--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesDetailComponent
   ],
   imports: [
     BrowserModule,
