4517462425aeaf98d248616c4c8b9429d6fc0049
Ken-muigai <kenmuigai980@gmail.com>
2020-08-24 05:55:40 +0300

    modifications

--- quotes/src/app/vote-count.directive.ts
+++ quotes/src/app/vote-count.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appVoteCount]'
@@ -7,8 +7,21 @@ export class VoteCountDirective {
   upVotes:number =  0;
   downVotes: number = 0;
 
-  
 
+  @HostListener('click') onclick(){
+    this.counter(null)
+  }
+
+  counter(value: boolean){
+    if(value){
+      console.log(this.upVotes++)
+    }else if(!value){
+      console.log(this.downVotes++)
+    }else{
+      return 0
+    }
+
+  }
 
 
   constructor(private elem:ElementRef) {
