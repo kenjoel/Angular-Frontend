6bed41ad448be7984efe653a6e63c85fa9982c50
Ken-muigai <kenmuigai980@gmail.com>
2020-08-24 14:56:36 +0300

    final commits

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
     new quotes("“The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.”","Michael Cain", "kenjoel",new Date(2020,1,29)),
     new quotes("“When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.”"
     , "lex fridman", "Pres. Joel Muigai",new Date(2019,5,6)),
-    new quotes("“A successful man is one who can lay a firm foundation with the bricks others have thrown at him.”", "David Brinkly","KenJoel Muigai", new Date(2020,7,21))
+    new quotes("“A successful man is one who can lay a firm foundation with the bricks others throw at him.”", "David Brinkly","KenJoel Muigai", new Date(2020,7,21))
   ]
 
   showDetails(index){
